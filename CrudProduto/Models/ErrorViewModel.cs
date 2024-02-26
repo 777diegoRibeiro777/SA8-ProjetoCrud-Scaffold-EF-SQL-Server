b8aac5b942791f2138002297d22517d2a9f3cdad
Diego Ribeiro <51891308+777diegoRibeiro777@users.noreply.github.com>
2024-02-26 15:13:30 -0300

    Adicionar arquivos de projeto.

---
+++ CrudProduto/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace CrudProduto.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
---
+++ CrudProduto/wwwroot/js/site.js
@@ -0,0 +1,17 @@
+﻿let tableProduto = new DataTable('#Table_Produto', {
+    language: {
+        url: '//cdn.datatables.net/plug-ins/2.0.0/i18n/pt-BR.json',
+    },
+});
+
+let tableCategoria = new DataTable('#Table_Categoria', {
+    language: {
+        url: '//cdn.datatables.net/plug-ins/2.0.0/i18n/pt-BR.json',
+    },
+});
+
+let tableFornecedores = new DataTable('#Table_Fornecedores', {
+    language: {
+        url: '//cdn.datatables.net/plug-ins/2.0.0/i18n/pt-BR.json',
+    },
+});
